7ed351104d6c9e346457caf888747d4ea150033d
andreoddone <98482734+andreoddone@users.noreply.github.com>
2024-09-05 16:54:32 -0300

    Create morador.java

---
+++ morador.java
@@ -0,0 +1,17 @@
+public class Morador {
+    private String nome;
+    private String apartamento;
+
+    public Morador(String nome, String apartamento) {
+        this.nome = nome;
+        this.apartamento = apartamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getApartamento() {
+        return apartamento;
+    }
+}
